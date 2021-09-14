92ee6b77618a1c8de179b24fffb227aaabe200ad
Aman Sultan Baig <amansultanbaig@gmail.com>
2021-09-14 22:39:22 +0500

    user table ui completed

--- client/src/pages/userTable.js
+++ client/src/pages/userTable.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import { Popconfirm, message } from 'antd';
 
 function userTable() {
 
+    function confirm(e) {
+        console.log(e);
+    }
+
+    function cancel(e) {
+        console.log(e);
+    }
+
     let userData = [
-        {id: 1,name: "User1", email: "user1@gmail.com", contact: "12343233", class: "1", courses: ["Eng", "Math"]}
+        {
+            id: 1,
+            name: "User1",
+            email: "user1@gmail.com",
+            contact: "12343233",
+            class: "1",
+            courses: ["Eng", "Math"]
+        },
+        {
+            id: 1,
+            name: "User1",
+            email: "user1@gmail.com",
+            contact: "12343233",
+            class: "1",
+            courses: ["Eng", "Math"]
+        },
+        {
+            id: 1,
+            name: "User1",
+            email: "user1@gmail.com",
+            contact: "12343233",
+            class: "1",
+            courses: ["Eng", "Math"]
+        },
+        {
+            id: 1,
+            name: "User1",
+            email: "user1@gmail.com",
+            contact: "12343233",
+            class: "1",
+            courses: ["Eng", "Math"]
+        },
     ]
 
     return (
@@ -17,26 +57,37 @@ function userTable() {
                                 <th scope="col">Name</th>
                                 <th scope="col">Email</th>
                                 <th scope="col">Contact</th>
+                                <th scope="col">Class</th>
+                                <th scope="col">Courses</th>
+                                <th scope="col"></th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td colspan="2">Larry the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
+                            {
+                                userData.map(item => (
+                                    <tr>
+                                        <th scope="row">{item.id}</th>
+                                        <th>{item.name}</th>
+                                        <th>{item.email}</th>
+                                        <th>{item.contact}</th>
+                                        <th>{item.class}</th>
+                                        <th>{item.courses.join(",")}</th>
+                                        <th>
+                                            <a href="#">Edit</a>
+                                            &nbsp;&nbsp;
+                                            <Popconfirm
+                                                title="Are you sure to delete this task?"
+                                                onConfirm={confirm}
+                                                onCancel={cancel}
+                                                okText="Yes"
+                                                cancelText="No">
+                                                <a href="#">Delete</a>
+                                            </Popconfirm>
+                                        </th>
+                                    </tr>
+                                ))
+                            }
+
                         </tbody>
                     </table>
                 </div>
